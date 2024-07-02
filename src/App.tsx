6110e45f8354e55edfac4d1231ad732f28c43965
plusbeauxjours <plusbeauxjours@gmail.com>
2024-07-02 22:07:09 +0700

    set: LoadingScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import LoadingScreen from "./components/loading-screen";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
 
   return (
-    <div/>
-  )
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  );
 }
 
 export default App;
