246a97b9c8695128a39ca9b314d274b5128827f9
plusbeauxjours <plusbeauxjours@gmail.com>
2024-07-02 23:19:21 +0700

    set: Create Account

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
+
+import { auth } from "../firebase";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -42,6 +46,7 @@ const Error = styled.span`
 `;
 
 export default function CreateAccount() {
+    const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -59,12 +64,21 @@ export default function CreateAccount() {
       setPassword(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
     try {
-      // create an account
-      // set the name of the user.
-      // redirect to the home page
+        setLoading(true);
+        const credentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        console.log(credentials.user);
+        await updateProfile(credentials.user, {
+          displayName: name,
+        });
+        navigate("/");
     } catch (e) {
       console.log(e);
     } finally {
@@ -73,7 +87,7 @@ export default function CreateAccount() {
   };
   return (
     <Wrapper>
-      <Title>LogIn</Title>
+      <Title>Join XYZ</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
