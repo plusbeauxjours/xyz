173d4ab1063c4127650c38f56a474a8ebc806d21
plusbeauxjours <plusbeauxjours@gmail.com>
2024-07-04 23:38:37 +0700

    set: deleting tweets

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,8 @@
 import { styled } from "styled-components";
 import { ITweet } from "./timeline";
+import { deleteDoc, doc } from "firebase/firestore";
+import { auth, db, storage } from "../firebase";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
   display: grid;
@@ -31,12 +34,49 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
-export default function Tweet({ username, photo, tweet }: ITweet) {
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm("Are you sure you want to delete this tweet?");
+    if (!ok || user?.uid !== userId) return;
+    try {
+      // Document 삭제 (deleteDoc)
+      // document를 삭제하려면 delete() 메서드를 사용하세요.
+      // https://firebase.google.com/docs/firestore/manage-data/delete-data#delete_documents
+      await deleteDoc(doc(db, "tweets", id));
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+
+        // 파일 삭제 (deleteObject)
+        // 파일을 삭제하려면 먼저 해당 파일에 대한 reference를 만듭니다.
+        // 그런 다음 해당 reference에 대해 delete() 메서드를 호출합니다.
+        // https://firebase.google.com/docs/storage/web/delete-files#delete_a_file
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+    }
+  };
+
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
